6cc2046d730f37324cbd06022fd0215d0319f4b6
Max Vorobyev <gtvolk31@gmail.com>
2021-04-02 19:25:51 +0300

    Fix

--- src/main/java/org/bdshadow/interview/MoneyService.java
+++ src/main/java/org/bdshadow/interview/MoneyService.java
@@ -5,11 +5,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.atomic.AtomicIntegerArray;
+import java.util.concurrent.atomic.AtomicInteger;
 
-/**
- * FIX THIS SERVICE
- */
 @Service
 @RequiredArgsConstructor
 public class MoneyService {
@@ -17,14 +14,14 @@ public class MoneyService {
     private static final int HARDCODED_NUMBER_OF_CANDIDATES = 100;
     private final CandidateService candidateService;
 
+    @SuppressWarnings("unchecked")
     public int countMoney() throws ExecutionException, InterruptedException {
-        AtomicIntegerArray result = new AtomicIntegerArray(1);
-        result.set(0, 0);
-        CompletableFuture[] futures = new CompletableFuture[HARDCODED_NUMBER_OF_CANDIDATES];
+        AtomicInteger result = new AtomicInteger();
+        CompletableFuture<Void>[] futures = new CompletableFuture[HARDCODED_NUMBER_OF_CANDIDATES];
         for (int i = 1; i <= HARDCODED_NUMBER_OF_CANDIDATES; i++) {
-            futures[i - 1] = candidateService.getMoney(i).thenAccept(money -> result.addAndGet(0, money));
+            futures[i - 1] = candidateService.getMoney(i).thenAccept(money -> result.addAndGet(money));
         }
         CompletableFuture.allOf(futures).get();
-        return result.get(0);
+        return result.get();
     }
 }
